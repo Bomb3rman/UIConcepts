ba26aa8ca7d224cf7cf45f71b78a46cc5a343f98
Fabian Bumberger <fbumberger@rim.com>
2013-04-25 13:47:08 +0200

    Refactored the Woodprofiles model to fit the actual use case

--- datahandling/wood.cpp
+++ datahandling/wood.cpp
@@ -1,5 +1,55 @@
 #include "wood.h"
 
+
+Defect::Defect(const QString &name, const qreal &threshold, const DType type, const QString &description, QObject *parent)
+    : QObject(parent), m_name(name), m_threshold(threshold), m_type(type), m_description(description)
+{
+
+}
+
+QString Defect::name() const
+{
+    return m_name;
+}
+
+void Defect::setName(const QString &name)
+{
+    m_name = name;
+    Q_EMIT nameChanged();
+}
+
+Defect::DType Defect::type() const
+{
+    return m_type;
+}
+
+void Defect::setType(const DType &type)
+{
+    m_type = type;
+    Q_EMIT typeChanged();
+}
+
+qreal Defect::threshold() const
+{
+    return m_threshold;
+}
+
+void Defect::setThreshold(const qreal &threshold)
+{
+    m_threshold = threshold;
+}
+
+QString Defect::description() const
+{
+    return m_description;
+}
+
+void Defect::setDescription(const QString &description)
+{
+    m_description = description;
+    Q_EMIT descriptionChanged();
+}
+
 Wood::Wood(const QString &name)
     : m_name(name)
 {
@@ -9,3 +59,14 @@ QString Wood::name() const
 {
     return m_name;
 }
+
+QList<QObject *> Wood::defects() const
+{
+    return m_defects;
+}
+
+void Wood::appendDefect(Defect *defect)
+{
+    m_defects.append((QObject*)defect);
+}
+
--- datahandling/wood.h
+++ datahandling/wood.h
@@ -5,18 +5,60 @@
 #include <QImage>
 #include <QUrl>
 
+class Defect : QObject{
+    Q_OBJECT
+
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(DType type READ type WRITE setType NOTIFY typeChanged)
+    Q_PROPERTY(qreal threshold READ threshold WRITE setThreshold NOTIFY thresholdChanged)
+    Q_PROPERTY(QString description READ description WRITE setDescription NOTIFY descriptionChanged)
+
+public:
+
+    enum DType {
+        LessThan,
+        LongerThan
+    };
+
+    Defect(const QString &name, const qreal &threshold, const DType type, const QString &description = "", QObject *parent=0);
+
+    QString name() const;
+    void setName(const QString &name);
+
+    DType type() const;
+    void setType(const DType &type);
+
+    qreal threshold() const;
+    void setThreshold(const qreal &threshold);
+
+    QString description() const;
+    void setDescription(const QString &description);
+
+signals:
+    void nameChanged();
+    void typeChanged();
+    void thresholdChanged();
+    void descriptionChanged();
+
+private:
+    QString m_name;
+    qreal m_threshold;
+    DType m_type;
+    QString m_description;
+};
+
 class Q_DECL_EXPORT Wood
 {
 public:
     Wood(const QString &name);
 
     QString name() const;
+    QList<QObject *> defects() const;
+    void appendDefect(Defect *defect);
 
 private:
     QString m_name;
-    float velocity;
-    float quality;
-    float weight;
+    QList<QObject *> m_defects;
 
 };
 #endif // WOOD_H
